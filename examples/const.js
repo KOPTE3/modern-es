cc8580d461e79b4855a3e7768715a50ba5e7f435
Anatoliy Ostapenko <ostapencko.anatoliy@yandex.ru>
2016-10-15 08:01:17 +0300

    Add slides

---
+++ examples/const.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const v1 = 42;
+const v2 = [1, 2];
+const v3 = {foo: 'bar'};
+const v4;
---
+++ examples/var.js
@@ -0,0 +1,10 @@
+'use strict';
+
+if (true) {
+	var value1 = 42;
+}
+
+console.log(value1);
+console.log(value2);
+
+var value2 = 42;
